d3956323bede1414b1815d23ca15d60ff7139c1b
José Luiz Mendes <braabozeca@gmail.com>
2025-12-18 14:21:01 +0000

    style: Update button variants in PaginationLink component

--- wedding-new/src/_components/ui/button.tsx
+++ wedding-new/src/_components/ui/button.tsx
@@ -19,6 +19,8 @@ const buttonVariants = cva(
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
+        foreground: 
+          "hover:bg-foreground hover:text-zinc-50 dark:hover:bg-foreground/50",
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
