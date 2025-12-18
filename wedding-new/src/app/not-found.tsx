f74db85391f28396bb52d0614fe3ca0541dc3478
H1PEZECA <braabozeca@gmail.com>
2025-12-17 23:27:28 -0300

    feat: Add a custom 404 Not Found page.

--- wedding-new/src/app/not-found.tsx
+++ wedding-new/src/app/not-found.tsx
@@ -30,35 +30,34 @@ const NotFound = () => {
                     animate={{ opacity: 1, y: 0 }}
                     transition={{ duration: 0.8 }}
                 >
-                    <h1 className="playfair-custom text-8xl md:text-9xl mb-4 text-zinc-50 font-bold">
+                    <h1 className="playfair-custom text-8xl md:text-9xl mb-4 text-zinc-50">
                         404
                     </h1>
-
                     <div className="w-24 h-1 bg-zinc-50 mx-auto mb-8" />
-
-                    <p className="text-2xl md:text-3xl text-zinc-50 mb-4 font-light">
-                        Página não encontrada
-                    </p>
-
-                    <p className="text-lg md:text-xl text-zinc-200 mb-12 max-w-2xl mx-auto">
-                        Opa! Parece que o você se perdeu no caminho para o altar.
-                    </p>
-
-                    <motion.div
-                        className="flex justify-center"
-                        whileHover={{ scale: 1.05 }}
-                        whileTap={{ scale: 0.95 }}
-                    >
-                        <Link href="/">
-                            <Button
-                                size="lg"
-                                className="group text-lg shadow-xl py-8 px-8 bg-zinc-50 text-foreground hover:bg-zinc-200 transition-colors duration-300"
-                            >
-                                <ArrowLeft className="mr-2 h-6 w-6 transition-transform group-hover:-translate-x-1" />
-                                Voltar para o Início
-                            </Button>
-                        </Link>
-                    </motion.div>
+                    <div className="text-center playfair-custom">
+                        <p className="text-2xl md:text-3xl text-zinc-50 mb-4 ">
+                            Página não encontrada
+                        </p>
+                        <p className="text-lg md:text-xl text-zinc-50 mb-12 max-w-2xl mx-auto">
+                            Opa! Parece que o você se perdeu no caminho para o altar.
+                        </p>
+                        <motion.div
+                            className="flex justify-center"
+                            whileHover={{ scale: 1.05 }}
+                            whileTap={{ scale: 0.95 }}
+                        >
+                            <Link href="/">
+                                <Button
+                                variant={"ghost"}
+                                    size="lg"
+                                    className="group text-lg shadow-xl py-8 px-8 bg-zinc-50 hover:text-foreground hover:bg-zinc-200 transition-colors duration-300 font-semibold"
+                                >
+                                    <ArrowLeft className="mr-2 mt-0.5 h-6 w-6 transition-transform group-hover:-translate-x-1 left-1/2 -translate-x-1/2 animate-bounce" />
+                                    Voltar para o Início
+                                </Button>
+                            </Link>
+                        </motion.div>
+                    </div>
                 </motion.div>
             </div>
         </section>
